d1736dae585b74dbf58bce8adce2ed878bd6f41b
Niisango <cafu_chino@outlook.jp>
2026-01-03 21:17:15 +0900

    update

--- packages/frontend/src/store.ts
+++ packages/frontend/src/store.ts
@@ -483,7 +483,7 @@ export const store = markRaw(new Pizzax('base', {
 	},
 	setFederationAvatarShape: {
 		where: 'account',
-		default: true,
+		default: false,
 	},
 	filesGridLayoutInUserPage: {
 		where: 'device',
